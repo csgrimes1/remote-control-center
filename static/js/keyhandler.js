cfe48ce23a85ec48667ab9e37212ed5fb25bd121
Chris Grimes <csgrimes1@gmail.com>
2018-03-09 15:14:43 -0800

    Key changes.

--- static/js/keyhandler.js
+++ static/js/keyhandler.js
@@ -1,10 +1,16 @@
 document.onkeydown = (e) => {
-    const el = document.getElementById(e.code)
+    const id = `${e.ctrlKey ? 'ctrl-' : ''}${e.code}`
+    const el = document.getElementById(id)
     if (el) {
         e.preventDefault()
         e.stopPropagation()
         e.stopImmediatePropagation()
         console.log('code:', e.code)
-        setTimeout(() => el.click(), 0)
+        setTimeout(() => {
+            el.focus()
+            el.click()
+        }, 0)
+        return false
     }
+    return true
 }
