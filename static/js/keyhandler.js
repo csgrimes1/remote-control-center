cca5aec33bfb0f4377d69495c4e3a25a6a275e67
Chris Grimes <csgrimes1@gmail.com>
2018-03-09 14:26:10 -0800

    Eliminated event bubbling on keypress.

--- static/js/keyhandler.js
+++ static/js/keyhandler.js
@@ -1,6 +1,9 @@
 document.onkeydown = (e) => {
     const el = document.getElementById(e.code)
     if (el) {
+        e.preventDefault()
+        e.stopPropagation()
+        e.stopImmediatePropagation()
         console.log('code:', e.code)
         setTimeout(() => el.click(), 0)
     }
